4f2d9bee08c5757165493061480fd7bcf4ca3a65
Serhii <zyozya8@gmail.com>
2020-04-17 23:52:43 +0300

    add  to tests

--- Gemfile
+++ Gemfile
@@ -8,6 +8,7 @@ gem 'rake'
 group :test do
   gem 'rspec'
   gem 'rack-test'
+  gem 'database_cleaner-active_record'
 end
 
 gem "sinatra-contrib", "~> 2.0"
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,22 +3,20 @@ ENV['APP_ENV'] = 'test'
 require './app'
 require 'rspec'
 require 'rack/test'
+require 'database_cleaner/active_record'
 
 RSpec.configure do |config|
   config.include Rack::Test::Methods
   config.before(:all) do
     load "./db/schema.rb"
   end
-  config.before(:each) do
-    tables = [
-      'restaurants',
-      'tables',
-      'users',
-      'reservations',
-    ]
-    tables.each do |table_name|
-      ActiveRecord::Base.connection.execute("Delete from #{table_name}")
-      ActiveRecord::Base.connection.execute("DELETE FROM SQLITE_SEQUENCE WHERE name='#{table_name}'")
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+  end
+  config.around(:each) do |example|
+    DatabaseCleaner.cleaning do
+      example.run
     end
-  end 
+  end
 end
